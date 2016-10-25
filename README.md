46115657526bffaee931b9c8fcc629eb17462fa9
fenix <fenixdeveloper@gmail.com>
2016-10-25 09:55:08 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# queue
+# RabbitMQ wrapper for PubSub, RPC, Queue based on official examples for amqplib
+
+## Init
+
+```javascript
+var RabbitMQ = require('env-rmq').RabbitMQ;
+
+var rmq = new RabbitMQ(config);
+rmq.connect(function () {
+	debug('connected');
+	callback(null, rmq);
+});
+```
+
+## Queue
+
+### Worker - handle task
+
+```javascript
+rmq.worker('my-queue-name', function (msg, reject, ack) {
+	ack();
+});
+```
+
+### Client - send task
+
+```javascript
+rmq.worker('my-queue-name').send({
+	someField: "Hello world!"
+});
+```
+
+## PubSub
+
+### Listener
+
+```javascript
+rmq.pubsub('my-pub-name', function (msg, reject, ack) {
+	ack();
+});
+```
+
+### Sender
+
+```javascript
+rmq.pubsub('my-pub-name').send({
+	someField: "Hello world!"
+});
+```
+
+## RPC
+
+### Server
+
+```javascript
+rmq.rpc('my-function', function (msg, reject, ack) {
+	ack({
+		returnValue:"OK!"
+	});
+});
+```
+
+### Client
+
+```javascript
+rmq.pubsub('my-function').call({
+	someField: "Hello world!"
+}, function (result) {
+	console.log("RETURNED VALUE:", result);
+});
+```
---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict'
+
+var debug 	= require('debug')('env:rabbitmq');
+var amqp = require('amqplib/callback_api');
+
+var Worker = require('./lib/worker');
+var Rpc = require('./lib/rpc');
+var PubSub = require('./lib/pubsub');
+
+class RabbitMQ {
+	constructor(_config) {
+		this.config = _config;
+		this.connection = null;
+		this.queue = new Map();
+	}
+
+	connect(_ready) {
+		amqp.connect(this.config.uri, (function (_err, _conn) {
+			if (_err) {
+				return _ready(new Error("Can not connect to RabbitMQ"));
+			} else {
+				this.connection = _conn;
+				_ready(null);
+			}
+		}).bind(this));
+	}
+
+	worker(_name, _handler) {
+		if (this.connection == null) return;
+
+		if (this.queue.has(_name)) {
+			if (_handler) {
+				return new Error("Queue already in use");
+			} else {
+				return this.queue.get(_name);
+			}
+		}
+		this.queue.set(_name, new Worker(_name, _handler));
+
+		return this.queue.get(_name).register(this.connection);
+	}
+
+	rpc(_name, _handler) {
+		if (this.connection == null) return;
+
+		if (this.queue.has(_name)) {
+			if (_handler) {
+				return new Error("Queue already in use");
+			} else {
+				return this.queue.get(_name);
+			}
+		}
+		this.queue.set(_name, new Rpc(_name, _handler));
+
+		return this.queue.get(_name).register(this.connection);
+	}
+
+	pubsub(_name, _handler) {
+		if (this.connection == null) return;
+
+		if (this.queue.has(_name)) {
+			if (_handler) {
+				return new Error("Queue already in use");
+			} else {
+				return this.queue.get(_name);
+			}
+		}
+		this.queue.set(_name, new PubSub(_name, _handler));
+
+		return this.queue.get(_name).register(this.connection);
+	}
+}
+
+module.exports.RabbitMQ = RabbitMQ;
